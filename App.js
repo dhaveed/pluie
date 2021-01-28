05c4dd61234343d37f85e0df3c3a3b8c9b5e480b
dhaveed <davidadegoke31@gmail.com>
2021-01-28 15:09:00 +0100

    Added Work Summary and Todos

--- App.js
+++ App.js
@@ -19,3 +19,25 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 });
+
+/*
+  Day 1:
+    Time Spent => 2hrs [1pm - 3pm]
+    Tasks Completed => {
+      1. Project and git repo setup
+      2. Files and Directories Setup
+      3. Core Dependencies Installed [navigation, icons, appearance for themeing etc]
+      4. All screens basic structure and components created [Cities, Settings, Forecast, Weather Screens]
+    }
+  _____________________________________________________________________________________
+
+  What's left to do?
+   [] Set up and configure navigation
+   [] Set up and configure navigation headers
+   [] Set up and configure redux/redux-thunk/async-storage
+   [] Set up and hookup theming functionality
+   [] Retrieve live weather data from API on weather screen
+   [] Retrieve live data on Forecast screen
+   [] Connect all Settings options as appropriate
+   [] Cities "add" functionality
+*/
